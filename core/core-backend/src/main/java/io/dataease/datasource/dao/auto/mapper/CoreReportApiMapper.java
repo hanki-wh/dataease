9b3086b40162139c7b1e7279515133fa18593229
lining <lining@houseitech.com>
2024-07-29 18:01:09 +0800

    Update report api

---
+++ core/core-backend/src/main/java/io/dataease/datasource/dao/auto/mapper/CoreReportApiMapper.java
@@ -0,0 +1,19 @@
+package io.dataease.datasource.dao.auto.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.dataease.datasource.dao.auto.entity.CoreDatasource;
+import io.dataease.datasource.dao.auto.entity.CoreReportApi;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author fit2cloud
+ * @since 2023-09-26
+ */
+@Mapper
+public interface CoreReportApiMapper extends BaseMapper<CoreReportApi> {
+
+}
