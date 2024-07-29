e6ea695d9959efa0e7b69770aa6dd6608b0c4708
songyazhu <songyazhu@houseitech.com>
2024-07-29 22:24:15 +0800

    update API

--- core/core-frontend/src/api/visualization/visualizationBackground.ts
+++ core/core-frontend/src/api/visualization/visualizationBackground.ts
@@ -3,4 +3,5 @@ import request from '@/config/axios'
 export const queryVisualizationBackground = () =>
   request.get({ url: '/visualizationBackground/findAll' })
 
-export const queryAllReportApi = () => request.get({ url: '/queryAllReportApi/' })
+export const queryAllReportApi = () => request.get({ url: '/datasource/queryAllReportApi' })
+export const downloadOneFile = (fileUrl: any) => request.get({ url: fileUrl })
--- core/core-frontend/src/custom-component/de-button/Component.vue
+++ core/core-frontend/src/custom-component/de-button/Component.vue
@@ -1,19 +1,32 @@
 <template>
   <el-button
     v-if="propValue['url']"
-    @click="downloadFile()"
+    @click="downloadFile(propValue['url'])"
     style="margin-right: 12px"
     type="primary"
   >
     Button1
   </el-button>
-  <el-button v-else @click="queryAllReportInfo()" style="margin-right: 12px" type="primary">
-    Button2
-  </el-button>
+  <el-dropdown v-else trigger="hover">
+    <el-button @click="queryAllReportInfo()" style="margin-right: 12px" type="primary">
+      Button2
+    </el-button>
+    <template #dropdown>
+      <el-dropdown-menu class="drop-style">
+        <el-dropdown-item
+          v-for="item in fileUrlList"
+          :key="item.id"
+          @click="updateUrl(item.apiUrl)"
+        >
+          {{ item.name }}
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
 </template>
 
 <script setup lang="ts">
-import { queryAllReportApi } from '@/api/visualization/visualizationBackground'
+import { downloadOneFile, queryAllReportApi } from '@/api/visualization/visualizationBackground'
 import { reactive, toRefs } from 'vue'
 const props = defineProps({
   propValue: {
@@ -31,27 +44,21 @@ const props = defineProps({
   }
 })
 const { propValue } = toRefs(props)
-const state = reactive({
-  fileUrlList: {}
-})
-// const newComponent = componentName => {
-//   eventBus.emit('handleNew', { componentName: componentName, innerType: componentName })
-// }
-const downloadFile = () => {
-  const fileUrl = 'https://example.com/path/to/file.pdf'
-  const link = document.createElement('a')
-  link.href = fileUrl
-  link.download = 'file.pdf'
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
+let fileUrlList: any = reactive([])
+const updateUrl = (url: any) => {
+  propValue.value['url'] = url
+}
+const downloadFile = (url: any) => {
+  downloadOneFile(url).then(res => {
+    return res?.data
+  })
 }
 
 const queryAllReportInfo = () => {
   queryAllReportApi().then(response => {
-    state.fileUrlList = response.data
+    fileUrlList = response.data
   })
-  console.log('file:' + state.fileUrlList)
+  console.log('file:' + fileUrlList)
 }
 </script>
 
