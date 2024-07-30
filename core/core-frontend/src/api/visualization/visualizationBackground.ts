5df5edcd70d28a988c9c1aaa9da0643a6aa16f23
songyazhu <songyazhu@houseitech.com>
2024-07-30 13:20:22 +0800

    Fix download

--- core/core-frontend/src/api/visualization/visualizationBackground.ts
+++ core/core-frontend/src/api/visualization/visualizationBackground.ts
@@ -4,5 +4,5 @@ export const queryVisualizationBackground = () =>
   request.get({ url: '/visualizationBackground/findAll' })
 
 export const queryAllReportApi = () => request.get({ url: '/datasource/queryAllReportApi' })
-export const downloadOneFile = async (fileUrl: any): Promise<Blob> =>
+export const downloadOneFile = async (fileUrl: any): Promise<IResponse> =>
   request.get({ url: fileUrl, responseType: 'blob' })
--- core/core-frontend/src/custom-component/de-button/Component.vue
+++ core/core-frontend/src/custom-component/de-button/Component.vue
@@ -45,7 +45,7 @@ const updateUrl = (item: any) => {
 }
 const downloadFile = (item: any) => {
   downloadOneFile(item.url).then(res => {
-    const blob = new Blob([res], { type: 'text/plain' })
+    const blob = new Blob([res.data], { type: 'text/plain' })
     const link = document.createElement('a')
     link.style.display = 'none'
     link.href = URL.createObjectURL(blob)
